62b23d573f7df5f6b6a09aa92e9d3973f19b80ec
Doppler-Effect <ximik827@gmail.com>
2013-02-18 23:49:25 +0400

    Made some MSProject work overnight

--- MSProjectLib/MSProjectInterop.cs
+++ MSProjectLib/MSProjectInterop.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using MSProject = Microsoft.Office.Interop.MSProject;
 
 namespace MSProjectLib
@@ -11,30 +12,46 @@ namespace MSProjectLib
     {
         MSProject.Application application;
 
-        public void LaunchProject()
+        public MSProjectInterop()
         {
-            this.application = new MSProject.Application();
-            bool launched = false;
+            this.LaunchMSProject();
+        }
 
-            for (int timerSec = 0; timerSec < 20 && !launched; timerSec++)
+        private void LaunchMSProject()
+        {
+            try
             {
-                try
-                {
-                    this.application.WorkOffline(false);
-                    launched = true;
-                    break;
-                }
-                catch
-                {
-                }
+                this.application = new MSProject.Application();
+                application.Visible = true;
+                application.Projects.Add(System.Type.Missing, System.Type.Missing, System.Type.Missing);
+                #region Example code from http://social.technet.microsoft.com/Forums/ru-RU/project2010custprog/thread/0d4b2b34-3051-4b27-bb51-e3cae64ac9f9
+                //bool launched = false;
+                //for (int timerSec = 0; timerSec < 20 && !launched; timerSec++)
+                //{
+                //    try
+                //    {
+                //        this.application.WorkOffline(false);
+                //        launched = true;
+                //        break;
+                //    }
+                //    catch
+                //    {
+                //    }
+                //}
+
+                //if (!launched)
+                //{
+                //    application.DocClose();
+                //    System.Threading.Thread.Sleep(1500);
+                //    application.Quit(MSProject.PjSaveType.pjDoNotSave);
+                //    throw new ApplicationException("Unable to start an instance of MS Project");
+                //} 
+                #endregion
+                               
             }
-
-            if (!launched)
+            catch (Exception Ex)
             {
-                application.DocClose();
-                System.Threading.Thread.Sleep(1500);
-                application.Quit(MSProject.PjSaveType.pjDoNotSave);
-                throw new ApplicationException("Unable to start an instance of MS Project");
+                MessageBox.Show(Ex.Message);
             }
         }
     }
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -11,7 +11,6 @@ namespace TestProject
         static void Main(string[] args)
         {
             MSProjectLib.MSProjectInterop msp = new MSProjectLib.MSProjectInterop();
-            msp.LaunchProject();
         }
     }
 }
